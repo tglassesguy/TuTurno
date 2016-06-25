fe0c4d671aa2cc11770dacea18fa5d670d393b1b
tglassesguy <jcollazosgomez@gmail.com>
2016-06-25 00:39:30 -0500

    agregando mas style

--- assets/js/main.js
+++ assets/js/main.js
@@ -303,4 +303,59 @@
 
 	});
 
+    $(function(){
+      margin = 10;
+      posicionInicial = 0;
+      dom = {}
+      st = {
+        stickyElement: '.stickyfloat_element',
+        footer : 'footer',
+      };
+      catchDom = function(){
+        dom.stickyElement = $(st.stickyElement);
+        dom.footer = $(st.footer);
+      }
+      afterCatchDom = function(){
+        functions.ubicarPosicionInicial()
+      }
+      suscribeEvents = function(){
+        $(window).on('scroll', events.moveStick);
+      }
+      events = {
+        moveStick : function(){
+
+          windowpos = $(window).scrollTop();
+          box = dom.stickyElement;
+          footer = dom.footer.offset();
+          console.log( footer.top - (box.height() + margin));
+          if ($(window).height() < footer.top - (windowpos + margin)) {
+            pos = windowpos + $(window).height() - (box.height() + margin);
+            dom.stickyElement.css({
+              top: pos + "px",
+              bottom: ''
+            });
+          } else{
+            pos = footer.top - (box.height() + margin);
+            dom.stickyElement.css({
+              top: pos + "px",
+              bottom: ''
+            });
+          }
+
+        }
+      }
+      functions = {
+        ubicarPosicionInicial : function(){
+          var newPosition = $(window).height() - (dom.stickyElement.height() + margin);
+          $(st.stickyElement).css('top', newPosition + "px");
+          posicionInicial = newPosition;
+        }
+      }
+      var init = function(){
+        catchDom();
+        afterCatchDom();
+        suscribeEvents();
+      };
+      init();
+    });
 })(jQuery);
--- assets/js/userturns.js
+++ assets/js/userturns.js
@@ -22,9 +22,9 @@ var notificar = "¡Es tu turno!";
 
 
 function app(){
-    
+
     userturn = Math.floor(Math.random()*7 + dataturn + 5);
-    document.getElementById("staticturn").innerHTML = userturn; 
+    document.getElementById("staticturn").innerHTML = userturn;
     document.getElementById("liveturn").innerHTML = dataturn;
     user(),
     mostrarpost(),
@@ -36,78 +36,78 @@ function app(){
 function onload(){
 
     cargarestado();
-    
+
 };
-    
+
     function cargarestado() {
-        
+
         firebase.database().ref("/status").on("value", function (snapshot){
-            
+
             status = snapshot.val();
             cargaractividad();
-            
+
         });
     };
-    
+
         function cargaractividad(){
-            
+
             firebase.database().ref("/actividad").on("value", function (snapshot) {
-                
+
                 actividad = snapshot.val();
-                cargardataturn();      
-                
+                cargardataturn();
+
             });
         };
             function cargardataturn() {
-                
+
                 firebase.database().ref("/console").on("value", function (snapshot) {
-                    
+
                     dataturn = snapshot.val();
                     cargarfaltan();
-                    
+
                 });
             };
                 function cargarfaltan() {
-                    
+
                     firebase.database().ref("/faltan").on("value", function(snapshot) {
-                        
+
                         faltan = snapshot.val();
                         cargaruserturn();
                     });
                 };
                     function cargaruserturn(){
-                        
+
                         firebase.database().ref("/turn").on("value", function (snapshot) {
-                            
+
                             userturn = snapshot.val();
                             comprobarestado();
                         });
                     };
-                    
+
 //////
 
 function comprobarestado() {
-    
+
     if (status == "false"){
-        
+
         ocultarboton(),
         ocultarpost(),
         mostraranuncio();
- 
+
     } if (status == "true" && actividad == false) {
-        
+
         ocultaranuncio(),
         ocultarpost(),
         mostrarboton();
-        
+
     } if (status == "true" && actividad == true) {
-        
+
         document.getElementById("staticturn").innerHTML = userturn;
         document.getElementById("liveturn").innerHTML = dataturn;
         ocultaranuncio(),
         mostrarpost(),
         ocultarboton();
-        
+
     };
 };
 
@@ -117,61 +117,61 @@ function subirlol(){
 
 
 function comprobarusario() {
-    
+
     firebase.database().ref("/actividad").on("value", function (snapshot) {
-        
+
         actividad = snapshot.val();
         unidos();
     });
 };
 
 function unidos() {
-    
+
     if (actividad == true){
-        
+
         firebase.database().ref("/turn").on("value", function (snapshot) {
-            
+
             ocultarboton(),
             mostrarpost();
             document.getElementById("staticturn").innerHTML = snapshot.val();
-            
+
         });
-        
+
         } else {
             mostrarboton();
         };
 };
 
 function user() {
-    
+
     actividad = true;
     firebase.database().ref("/turn").set(userturn),
     firebase.database().ref("/actividad").set(actividad);
-    
+
 };
 
 function ocultarboton() {
-    
+
     document.getElementById('boton').style.display = 'none';
 
 };
 
 function ocultarpost() {
-    
+
     document.getElementById('post').style.display = 'none';
 
 };
 
 function mostrarpost() {
-    
+
     document.getElementById("post").style.display = 'block';
-    
+
 };
 
 function mostrarboton() {
     document.getElementById("boton").style.display = 'block';
 };
-    
+
 function mostraranuncio() {
     document.getElementById("not").style.display = 'block';
 };
@@ -186,22 +186,31 @@ function pum(){
 };
 
 function subirultimolol() {
-    
+
     document.getElementById("comentario").style.display = none;
     document.getElementById("comentario_dos").innerHTML = notificar;
     document.getElementById("bonotes_exit").style.display = block;
 };
 
 function push() {
-    
+
     pull = true
-    
+
     if (pull == true){
-        
+
         pull = false;
         alert("Te faltan " + faltan + " turnos, \n ¡faltan pocos!.");
     } else {
         alert("lo logramos");
     };
-    
+
 };
+
+
+
+
+
+
+
+
+////
