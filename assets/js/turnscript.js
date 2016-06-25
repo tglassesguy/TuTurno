3be52cbbb5f31ca3941cda2cd0e3a4678304e160
javico <jcollazosgomez@gmail.com>
2016-06-25 04:26:32 +0000

    el sistema de turnos (depurar) ; agragando style

--- assets/js/turnscript.js
+++ assets/js/turnscript.js
@@ -1,17 +1,99 @@
-var console = 0;
+var console = 1;
 var status = false;
+var mostrar  = 0;
+var notificacion = 0;
+var notificacion_two = 0;
+var lol = 0;
+var ultimolol = 0;
+var gg = 0;
+var push_one = false;
+var push_two = false;
+var push_three = false;
+var final = 0;
+var empuja_one = 0;
+var empuja_two = 0;
+
+///
+
 var userturn = firebase.database().ref("/turn").on("value", function(snapshot) {
     
     userturn = snapshot.val();
     document.getElementById("userturn").innerHTML = userturn;
 });
+
+///
+
+var notificacion_one= firebase.database().ref("/turn").on("value", function(snapshot) {
+    
+    notificacion = Math.floor(snapshot.val() /2);
+    notificacion_two = Math.floor(notificacion/2);
+    document.getElementById("notification").innerHTML = notificacion;
+    document.getElementById("notification_dos").innerHTML = notificacion_two;
+    variables();
+
+});
+
+function variables() {
+    
+    empuja_one = notificacion;
+    empuja_two = notificacion_two;
+    
+};
+
+var comprobacionlol = firebase.database().ref("/faltan").on("value", function(snapshot) {
+
+    if (snapshot.val() == empuja_one){
+        
+        push_one= true;
+        firebase.database().ref("/push_one").set(push_one);
+        empuja_one ++;
+        
+    } if (snapshot.val() == notificacion_two){
+        
+        empuja_two ++;
+        push_two= true;
+        firebase.database().ref("/push_two").set(push_two);
+        
+    } if (snapshot.val() == final){
+        
+        final ++;
+        push_three= true;
+        firebase.database().ref("/push_three").set(push_three);
+    };
+});
+
+///
+
+var falta = firebase.database().ref("/console").on("value", function(snapshot) {
+    
+    mostrar = userturn - snapshot.val();
+    document.getElementById("falta").innerHTML = mostrar;
+    show();
+});
+
+///
+
+var finish = firebase.database().ref("/ultimolol").on("value" , function(snapshot) {
+    
+    ultimolol = snapshot.val();
+    finalizar();
+});
+
+function finalizar() {
+    
+    if(ultimolol == 1){
+        ultimolol --;
+        firebase.database().ref("/ultimolol").set(ultimolol);
+        reload();
+    };
+};
+
 var actividad = false;
+var firts= false;
 
 ///
 
 var segundos =  Math.floor(Math.random() * 7 + 10);
-var notificacionuno = Math.floor(turno / 2);
-var falta = (turno - liveturn);
 var bool = 0;
 var notbool = 0;
 var otrosegundos = 60;
@@ -32,7 +114,8 @@ function startTimer(duration, display) {
             timer = duration;
             console ++ ;
             document.getElementById("generalturn").innerHTML = console;
-            subir();
+            subir(),
+            notificacion();
         };
 
     }, 1000);
@@ -42,7 +125,7 @@ function startTimer(duration, display) {
 
 function iniciar(){
      
-    console = 0;
+    console = 1;
     document.getElementById("generalturn").innerHTML = console;
     status = true;
     estado(),
@@ -54,10 +137,8 @@ function iniciar(){
 
 function stop(){
     
-    renovar(), publish(), reload();
-    
     firebase.database().ref("/actividad").set(actividad);
-
+    renovar(), publish(), relolear(), repush(), reload();
 };
 
 
@@ -69,7 +150,7 @@ function reload() {
 
 function publish() {
     
-    console = 0;
+    console = 1;
     document.getElementById("generalturn").innerHTML = console;
     subir();
 };
@@ -118,7 +199,7 @@ function subir() {
 ///
 
 function onload() {
-
+    
     firebase.database().ref("/status").on('value', function(snapshot){
          status = snapshot.val();
          comprobar();
@@ -138,21 +219,52 @@ function comprobar() {
         });
         
     } else {
-        console = 0;
+        console = 1;
         document.getElementById("generalturn").innerHTML = console;
     };
 };
 
 
-
-
-
-function mostrar() {
-document.getElementById('oculto').style.display = 'block';
-};
-
-function ocultar() {
-document.getElementById('holo').style.display = 'none';
-};
-
 ///
+
+
+function notificacion() {
+    
+    if (notificacion == console){
+        
+        firts = true;
+        firebase.database().ref("/notificacion_one").set(notificacion_one);
+        alert("subimos la notifiacion");
+    };
+};
+
+
+function show() {
+    
+    firebase.database().ref("/faltan").set(mostrar);
+};
+function relolear() {
+    
+    ultimolol = 0;
+    
+    firebase.database().ref("/ultimolol").set(ultimolol);
+    
+    lol = 0;
+    
+    firebase.database().ref("/lol").set(lol);
+    
+    userturn = 0;
+    
+    firebase.database().ref("/turn").set(userturn);
+
+};
+
+function repush() {
+    
+    push_one = false;
+    push_two = false;
+    push_three = false;
+    firebase.database().ref("/push_one").set(push_one),
+    firebase.database().ref("/push_two").set(push_two),
+    firebase.database().ref("/push_three").set(push_three);
+};
--- assets/js/userturns.js
+++ assets/js/userturns.js
@@ -2,50 +2,119 @@ var dataturn = 0;
 var userturn = 0;
 var status = false;
 var actividad = false;
+var faltan = 0;
+var ultimolol = 0;
+var lol = 0;
+var pull = false;
+var push_one = firebase.database().ref("/push_one").on("value", function(snapshot) {
+    alert("Te faltan " + faltan + " turnos, \n ¡faltan pocos!.");
+});
+
+var push_two = firebase.database().ref("/push_two").on("value", function(snapshot) {
+    alert("Te faltan " + faltan + " turnos, \n ¡faltan pocos!.");
+});
+
+var push_three = firebase.database().ref("/push_two").on("value", function(snapshot) {
+    alert("¡es tu turno!")
+    subirultimolol();
+});
+var notificar = "¡Es tu turno!";
+
 
 function app(){
     
-    firebase.database().ref("/console").on("value", function (snapshot){
-        
-        dataturn = snapshot.val();
-    });
-    
-    userturn = Math.floor(Math.random()*7 + dataturn + 50);
+    userturn = Math.floor(Math.random()*7 + dataturn + 5);
     document.getElementById("staticturn").innerHTML = userturn; 
+    document.getElementById("liveturn").innerHTML = dataturn;
     user(),
     mostrarpost(),
     ocultarboton();
 };
 
+//////
+
 function onload(){
 
-    comprobarestado(),
-    comprobarusario();
+    cargarestado();
+    
 };
+    
+    function cargarestado() {
+        
+        firebase.database().ref("/status").on("value", function (snapshot){
+            
+            status = snapshot.val();
+            cargaractividad();
+            
+        });
+    };
+    
+        function cargaractividad(){
+            
+            firebase.database().ref("/actividad").on("value", function (snapshot) {
+                
+                actividad = snapshot.val();
+                cargardataturn();      
+                
+            });
+        };
+            function cargardataturn() {
+                
+                firebase.database().ref("/console").on("value", function (snapshot) {
+                    
+                    dataturn = snapshot.val();
+                    cargarfaltan();
+                    
+                });
+            };
+                function cargarfaltan() {
+                    
+                    firebase.database().ref("/faltan").on("value", function(snapshot) {
+                        
+                        faltan = snapshot.val();
+                        cargaruserturn();
+                    });
+                };
+                    function cargaruserturn(){
+                        
+                        firebase.database().ref("/turn").on("value", function (snapshot) {
+                            
+                            userturn = snapshot.val();
+                            comprobarestado();
+                        });
+                    };
+                    
+//////
 
 function comprobarestado() {
     
-    firebase.database().ref("/status").on("value", function (snapshot){
+    if (status == "false"){
         
-        status = snapshot.val();
-        estado();
-    });
-};
-
-function estado() {
-    
-    if (status == "true"){
-        
-        firebase.database().ref("/console").on("value", function (snapshot){
-            
-            document.getElementById("liveturn").innerHTML = snapshot.val();
-        });
-    } else {
-        ocultarpost(),
         ocultarboton(),
-        anuncio();
+        ocultarpost(),
+        mostraranuncio();
+ 
+    } if (status == "true" && actividad == false) {
+        
+        ocultaranuncio(),
+        ocultarpost(),
+        mostrarboton();
+        
+    } if (status == "true" && actividad == true) {
+        
+        document.getElementById("staticturn").innerHTML = userturn;
+        document.getElementById("liveturn").innerHTML = dataturn;
+        ocultaranuncio(),
+        mostrarpost(),
+        ocultarboton();
+        
     };
 };
+
+function subirlol(){
+    firebase.database().ref("/lol").set(lol);
+};
+
 
 function comprobarusario() {
     
@@ -84,13 +153,13 @@ function user() {
 function ocultarboton() {
     
     document.getElementById('boton').style.display = 'none';
-    
+
 };
 
 function ocultarpost() {
     
     document.getElementById('post').style.display = 'none';
-    
+
 };
 
 function mostrarpost() {
@@ -100,9 +169,39 @@ function mostrarpost() {
 };
 
 function mostrarboton() {
-    document.getElementById("boton").style.display = "block";
+    document.getElementById("boton").style.display = 'block';
 };
     
-function anuncio() {
-    document.getElementById("anuncio").innerHTML = "El servicio no está disponible";
+function mostraranuncio() {
+    document.getElementById("not").style.display = 'block';
+};
+
+function ocultaranuncio() {
+    document.getElementById("not").style.display = 'none';
+};
+
+
+function pum(){
+    alert("Te faltan " + faltan + " turnos, \n ¡faltan pocos!.");
+};
+
+function subirultimolol() {
+    
+    document.getElementById("comentario").style.display = none;
+    document.getElementById("comentario_dos").innerHTML = notificar;
+    document.getElementById("bonotes_exit").style.display = block;
+};
+
+function push() {
+    
+    pull = true
+    
+    if (pull == true){
+        
+        pull = false;
+        alert("Te faltan " + faltan + " turnos, \n ¡faltan pocos!.");
+    } else {
+        alert("lo logramos");
+    };
+    
 };
